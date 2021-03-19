de6e93abfd834f65ab1fd578cdb53bf521d7a745
William Maia <williamaiadias@hotmail.com>
2021-03-18 21:12:45 -0300

    Bugs resolvidos

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,6 +6,21 @@ describe('Utils', () => {
             expect(gerarNumeroAleatorio(20, -5))
                 .toBe(-1);
         });
+
+        test('inicio maior ou igual ao fim', () => {
+            expect(gerarNumeroAleatorio(20, 5))
+                .toBe(-1);
+        });
+
+        test('atribuição randomica', () => {
+            expect(gerarNumeroAleatorio(1, 20))
+                .toBeGreaterThanOrEqual(0);
+        });
+
+        test('inicio nao pode ser negativo', () => {
+            expect(gerarNumeroAleatorio(-4, 30))
+                .toBe(-1);
+        });
     });
 
     describe('acharCaracter', () => {
@@ -13,5 +28,22 @@ describe('Utils', () => {
             expect(acharCaracter(-4, 'abcde', 'c'))
                 .toBe('comprimento invalido');
         });
+
+        test('comprimento menor', () => {
+            expect(acharCaracter(2, 'abcde', 'c'))
+                .toBe('comprimento fornecido diferente do comprimento real');
+        });
+
+        test('busca de caracter', () => {
+            expect(acharCaracter(5, 'abcde', 'c'))
+                .toBe(2);
+        });
+
+        test('busca de caracter não existente', () => {
+            expect(acharCaracter(5, 'abcde', 'x'))
+                .toBe('caracter não encontrado');
+        });
     });    
+
+
 });
